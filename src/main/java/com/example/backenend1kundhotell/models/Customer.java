32627fd00de2e2dd2fa5120f59a08a61374920df
Kar Sar <sarkissova@gmail.com>
2025-05-19 00:58:41 +0200

    added a jsonignore to Booking

--- src/main/java/com/example/backenend1kundhotell/models/Customer.java
+++ src/main/java/com/example/backenend1kundhotell/models/Customer.java
@@ -3,8 +3,11 @@ package com.example.backenend1kundhotell.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -17,10 +20,15 @@ public class Customer {
 	private String lastName;
 	private String email;
 	private String phone;
+	private String booking;
 
-	public Customer(String firstName, String lastName, String email, String phone) {
+	public Customer(String firstName, String lastName, String email, String phone, String booking) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
+		this.phone = phone;
+		this.booking =booking;
 	}
+	@OneToMany(mappedBy = "booking")//bi-directional mapping.
+	private List<Booking> bookings;
 }
